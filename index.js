92ca08c93ac219e4cff5d8a19b8b3f1b62a6a891
Nawelchafi <chafinawel@gmail.com>
2024-02-14 01:18:05 +0100

    added the pictures of level one

---
+++ index.js
@@ -0,0 +1,13 @@
+const levelOnePics = [
+  { name: 'Acropolis', img: 'Acropolis.jpg' },
+  { name: 'Alhambra', img: 'Alhambra.jpg' },
+  { name: 'Belem Tower', img: 'Belem-Tower.jpg' },
+  { name: 'Colosseum', img: 'Colosseum.jpg' },
+  { name: 'Eiffel', img: 'Eiffel.jpg' },
+  { name: 'Fly Geyser', img: 'Fly-Geyser-Nevada.jpg' },
+  { name: 'Pyramide', img: 'Pyramide.jpg' },
+  { name: 'Hagia Sophia', img: 'Hagia-Sophia.jpg' },
+  { name: 'Rialto Bridge', img: 'Rialto-Bridge.jpg' },
+  { name: 'Sagrada Familia', img: 'Sagrada-Familia.jpg' },
+
+];
